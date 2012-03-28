8a16aa0f365344ac16aa700f5dde844b5b551958
Jonathan Bramble <j.p.bramble@leeds.ac.uk>
2012-03-28 16:40:51 +0100

    added edit

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,6 @@
 class Entry < ActiveRecord::Base
   belongs_to :language
   belongs_to :subtopic
-  has_many :translations
+  has_many :translations, :dependent => :destroy
   accepts_nested_attributes_for :translations
 end
