3ead63b30b21ee0036a2bd6c125ac5efaed5d7b7
Jonathan Bramble <j.p.bramble@leeds.ac.uk>
2012-04-02 17:24:36 +0100

    added update method

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,4 +3,8 @@ class Entry < ActiveRecord::Base
   belongs_to :subtopic
   has_many :translations, :dependent => :destroy
   accepts_nested_attributes_for :translations
+ 
+  def english
+	word
+  end
 end
